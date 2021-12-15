31b0c53896ed22343f9995671a514e002089cc24
FlYpPe <flype360360@gmail.com>
2021-12-14 22:21:06 -0600

    añadida api para android

--- API_REST_MySQL/api_consultas.php
+++ API_REST_MySQL/api_consultas.php
@@ -10,16 +10,37 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 
     if($cadenaJSON==false){
         echo "No Hay cadena JSON";
-    }else{
+    }else{ 
 
         $filtro = json_decode($cadenaJSON, true);
-        $sql = "SELECT * FROM Alumnos";
+        $a = $filtro['nc'];
+        $sql = "SELECT * FROM productos where IdProducto like '%$a%' or NombreProducto like '%$a%' or IdProveedor like '%$a%' 
+        or IdCategoria like '%$a%' or CantidadPorUnidad like '%$a%' or PrecioUnitario like '%$a%' 
+        or UnidadesEnStock like '%$a%' or Descripcion like '%$a%'";
         $res = mysqli_query($conexion,$sql);
-
         $datos ['alumnos'] = array();
         if($res){
             while($fila = mysqli_fetch_assoc($res)){
                 $alumno = array();
+                        /*
+        $id = $datos['id'];
+        $nom = $datos['nom'];
+        $prov = $datos['prov'];
+        $cat = $datos['cat'];
+        $cant = $datos['cant'];
+        $pre = $datos['pre'];
+        $stoc = $datos['stoc'];
+        $desc = $datos['desc'];
+
+                $producto = array();
+                $producto['id'] =$fila['IdProducto'];
+                $producto['nom'] =$fila['NombreProducto'];
+                $producto['prov'] =$fila['IdProveedor'];
+                $producto['cat'] =$fila['IdCategoria'];
+                $producto['cant'] =$fila['CantidadPorUnidad'];
+                $producto['pre'] =$fila['PrecioUnitario'];
+                $producto['stoc'] =$fila['UnidadesEnStock'];
+                $producto['desc'] =$fila['Descontinuado'];*/
 
                 $alumno['nc'] =$fila['Num_Control'];
                 $alumno['n'] =$fila['Nombre'];
@@ -32,6 +53,8 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
                 array_push($datos['alumnos'], $alumno);
             }
             echo json_encode($datos);
+
+
         }else{
             $respuesta['exito']= false;
             $respuesta['Mensaje']= "error en la consulta";
@@ -40,7 +63,63 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         }
 
 
-    }
+   }
+}
+
+?>
+
+<?php
+
+include('../scripts/php/conexiones_BD/conexion_bd_escuela.php');
+$con = new ConexionBDEscuela();
+$conexion = $con->getConexion();
+
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $cadenaJSON =file_get_contents('php://input');
+
+    if($cadenaJSON==false){
+        echo "No Hay cadena JSON";
+    }else{ 
+
+        $filtro = json_decode($cadenaJSON, true);
+        $a = $filtro['id'];
+        $sql = "SELECT * FROM productos where IdProducto like '%$a%'";
+        
+        $res = mysqli_query($conexion,$sql);
+        $datos ['objetos'] = array();
+        if($res){
+
+            while($fila = mysqli_fetch_assoc($res)){
+
+                                   
+                $producto = array();
+                $producto['id'] = $fila['IdProducto'];
+                $producto['nom'] =$fila['NombreProducto'];
+                $producto['prov'] =$fila['IdProveedor'];
+                $producto['cat'] =$fila['IdCategoria'];
+                $producto['cant'] =$fila['CantidadPorUnidad'];
+                $producto['pre'] =$fila['PrecioUnitario'];
+                $producto['stoc'] =$fila['UnidadesEnStock'];
+                $producto['desc'] =$fila['Descontinuado'];
+
+                
+                
+
+                array_push($datos['objetos'], $producto);
+            }
+            echo json_encode($datos);
+
+
+        }else{
+            $respuesta['exito']= false;
+            $respuesta['Mensaje']= "error en la consulta";
+            $resJSON = json_encode($respuesta);
+            echo json_encode($respuesta);
+        }
+
+
+   }
 }
 
 ?>

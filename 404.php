72fcac0bba79a7eee6a628453782f55a8b8aa889
FlYpPe <flype360360@gmail.com>
2021-12-14 22:19:40 -0600

    Agregada pantalla para 404

--- 404.php
+++ 404.php
@@ -1,12 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+	<meta charset="UTF-8">
+	<title>Error 404</title>
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+			font-family: sans-serif;
+            background: black;
+		}
+		body {
+			background-image: url('imgFondo.jpg');
+			width: 100%;
+			height: 100vh;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.container-404 {
+			vertical-align: bottom;
+			text-align: center;
+			width: 100%;
+			max-width: 1000px;
+			background: black;
+			padding: 100px;
+			border-radius: 7px;
+			line-height: 1.7;
+			color: #434343;
+		}
+		img{
+			vertical-align: top;
+			text-align: center;
+			width: 100%;
+			max-width: 700px;
+			padding: 0px;
+			border-radius: 5px;
+			line-height: 1.7;
+		}
+
+	</style>
+</head> 
+
 <body>
-    <h2>error</h2>
+	<div class="container-404">
+        <img src="imagenes/404.png" onClick="index.html">
+		
+	</div>
+
 </body>
+
 </html>
